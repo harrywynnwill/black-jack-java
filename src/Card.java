bfb1158a6dea9870be09842259252f9a537faf1e
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-08-12 13:35:13 +0100

    first commit

--- src/Card.java
+++ src/Card.java
@@ -1,9 +1,35 @@
+import java.util.*;
+
 public class Card {
-	
-	public Card(String rank, String suit, int value){
-		rank = rank;
-		suit = suit;
-		value = value;
-		
+
+	public enum Suit {
+		SPADES, HEARTS, DIAMONDS, CLUBS;
 	}
+
+	public enum Rank {
+		DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+	}
+
+	private final Rank rank;
+	private final Suit suit;
+	int value;
+	
+//    public Rank rank() { return rank; }
+//    public Suit suit() { return suit; }
+//    public String toString() { return rank + " of " + suit; }
+
+	 private Card(Rank rank, Suit suit) {
+	        this.rank = rank;
+	        this.suit = suit;
+	    }
+
+	private static final List<Card> deck = new ArrayList<Card>();
+	
+	static {
+		for(Suit suit : Suit.values())
+			for(Rank rank : Rank.values())
+				deck.add(new Card(rank, suit));
+	}
+		
+	
 }
--- src/Table.java
+++ src/Table.java
@@ -1,24 +1,32 @@
 
 public class Table {
+
 	
-	public static final String[] RANKS = {"A","2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+	public static Card[] deck = new Card[52];
+	public static int h = 1;
+	
 	
 	public Table(){
 		
 	}
 	public void MakeDeck(){
-		
+		for (Suits s: Suits.value()){
+			
+		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		Card card = new Card("harry", "is", 1);
 		Table table = new Table();
-//		System.out.print(table);
-		for (String card : RANKS){
-			System.out.println(card);
+		System.out.print(table.h);
+		for (String suit : SUITS)
+		for (
+			deck[i]=(new Card("harry", "was", VALUES[i]));
 		}
-		
+		for (Card card :deck){
+			System.out.println(card.value);
+		}
 	}
 
 }
